862936584ec036e627d12f9a19cdd45a7192e431
class moon <57883335+OrphanZhao@users.noreply.github.com>
2023-10-07 16:32:18 +0800

    dosc

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 require("express-async-errors");
+const fallback = require("express-history-api-fallback");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(fallback("index.html", { root: path.join(__dirname, "public") }));
 app.use(require("./middleware/initDatabase").initDatabase);
 
 app.use("/", indexRouter);
